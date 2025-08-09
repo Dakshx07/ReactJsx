b0da7933e91b0a6f758b6c3e45e5138dab807a51
Dakshx07 <daksh.hiran@gmail.com>
2025-08-09 12:32:06 +0530

    Error Boundary

--- More-Practice-2/More-React/src/App.jsx
+++ More-Practice-2/More-React/src/App.jsx
@@ -1,24 +1,65 @@
 
+import React from 'react';
 
 function App() {
-const todos=[{
-  title:"Learn React",
-  done:false,
-  key:1
-},{
-  title:"Learn Redux",
-  done:true,
-  key:2
-}]
-
-const TodoComponents=todos.map(todo => 
-  <Todo key={todo.key} title={todo.title} done={todo.done}/>
-)
-
   return <div>
-    {TodoComponents}
+      <ErrorBoundary>
+        <Card1/>
+      </ErrorBoundary>
+      <br />
+      <ErrorBoundary>
+        <Card2/>
+      </ErrorBoundary>
   </div>
 }
+
+function Card1(){
+
+  throw new Error("Hello")
+  return <div style={{background:"red",borderRadius:20,padding:20,margin:20}}>
+    hi there
+  </div>
+}
+
+function Card2(){
+  throw new Error("Hello")
+  return <div style={{background:"red",borderRadius:20,padding:20,margin:20}}>
+    Hello
+  </div>
+}
+
+
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, info) {
+        console.error("Error caught:", error, info);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <div style={{background:"red",borderRadius:20,padding:20,margin:20}}>
+    Something went wrong!  
+  </div>
+        }
+
+        return this.props.children; 
+    }
+}
+
+
+
+
+
+
 
 function Todo({title,done}){
   return <div>
